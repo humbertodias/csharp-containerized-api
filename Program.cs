5d45aaa9c1889a185d69766532bf448d2d8a050f
Humberto Dias <hldtux@gmail.com>
2023-12-17 23:26:10 -0300

    json response

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/ping", () => "pong");
+app.MapGet("/ping", () => new { Message = "pong" });
 
 app.Run();
